cc5389b0c1c298b04213cb879052b76ec7cec2b5
Khadar Mustafe <79355446+Khadar3344@users.noreply.github.com>
2024-12-26 22:59:08 +0300

    CustomDefaultBtn Updates

--- app/src/main/java/com/khadarmustafe/agroconsult/components/CustomDefaultBtn.kt
+++ app/src/main/java/com/khadarmustafe/agroconsult/components/CustomDefaultBtn.kt
@@ -24,7 +24,7 @@ fun CustomDefaultBtn(
     Button(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 20.dp, bottom = 20.dp)
+            .padding(10.dp)
             .height(50.dp)
             .clip(RoundedCornerShape(shapeSize.dp)),
         onClick = {
