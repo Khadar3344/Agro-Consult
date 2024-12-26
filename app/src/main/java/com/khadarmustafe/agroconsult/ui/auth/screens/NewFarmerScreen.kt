1b7863c0c7151c17c30bd742a3aa22ab5ce22d08
Khadar Mustafe <79355446+Khadar3344@users.noreply.github.com>
2024-12-26 08:40:02 +0300

    NewFarmerScreen added

--- app/src/main/java/com/khadarmustafe/agroconsult/ui/auth/screens/NewFarmerScreen.kt
+++ app/src/main/java/com/khadarmustafe/agroconsult/ui/auth/screens/NewFarmerScreen.kt
@@ -1,13 +1,20 @@
 package com.khadarmustafe.agroconsult.ui.auth.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,27 +23,71 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun NewFarmerScreen() {
+fun NewFarmerScreen(modifier: Modifier = Modifier) {
     var selectedAmount by remember { mutableStateOf("") }
-    var expandedAmount by remember { mutableStateOf(false) }
 
     var selectedFarmType by remember { mutableStateOf("") }
-    var expandedFarmType by remember { mutableStateOf(false) }
 
     var selectedCity by remember { mutableStateOf("") }
-    var expandedCity by remember { mutableStateOf(false) }
 
-    val investmentAmounts = listOf("1000 - 5000", "6000 - 10000", "11000 - 15000", "16000", "20000")
+    val investmentAmounts = listOf("1000 - 5000", "6000 - 10000", "11000 - 15000", "16000 - 20000", "21000 - 25000")
     val farmTypes = listOf("Dairy", "Poultry", "Crop", "Fishery")
     val cities = listOf("Hargeisa", "Gabilay", "Salaxlay", "Arabsiyo")
 
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Select Investment Details",
+            fontSize = 30.sp,
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        // Investment Amount Dropdown
+        DropdownSelector(
+            label = "Investment Amount",
+            options = investmentAmounts,
+            selectedOption = selectedAmount,
+            onOptionSelected = { selectedAmount = it }
+        )
+
+        // Farm Type Dropdown
+        DropdownSelector(
+            label = "Farm Type",
+            options = farmTypes,
+            selectedOption = selectedFarmType,
+            onOptionSelected = { selectedFarmType = it }
+        )
+
+        // City Dropdown
+        DropdownSelector(
+            label = "City",
+            options = cities,
+            selectedOption = selectedCity,
+            onOptionSelected = { selectedCity = it }
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = {
+            // Handle form submission
+        }) {
+            Text(text = "Submit")
+        }
+    }
+
 }
 
 @Composable
-fun DropdownSelector(
+private fun DropdownSelector(
     label: String,
     options: List<String>,
     selectedOption: String,
@@ -57,19 +108,20 @@ fun DropdownSelector(
                 }
             }
         )
-        /*DropdownMenu(
+        DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false }
         ) {
             options.forEach { option ->
-                DropdownMenuItem(onClick = {
-                    onOptionSelected(option)
-                    expanded = false
-                }) {
-                    Text(text = option)
-                }
+                DropdownMenuItem(
+                    text = {Text(text = option)},
+                    onClick = {
+                        onOptionSelected(option)
+                        expanded = false
+                    }
+                )
             }
-        }*/
+        }
     }
 }
 
