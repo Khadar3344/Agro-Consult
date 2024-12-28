e2df7ad3851c18c92852b8edde2641669a419bf8
Khadar Mustafe <79355446+Khadar3344@users.noreply.github.com>
2024-12-28 17:26:02 +0300

    SendSuccessfullyScreen added

---
+++ app/src/main/java/com/khadarmustafe/agroconsult/ui/home/screens/SendSuccessfullyScreen.kt
@@ -0,0 +1,32 @@
+package com.khadarmustafe.agroconsult.ui.home.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.khadarmustafe.agroconsult.ui.theme.primaryLight
+
+@Composable
+fun SendSuccessfullyScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Your Data Send successfully",
+            color = primaryLight,
+            textAlign = TextAlign.Center,
+            fontSize = 36.sp
+        )
+    }
+}
