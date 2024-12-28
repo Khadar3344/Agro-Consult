0bb68e68440ee11895c23d22f5afb4aff73619ec
Khadar Mustafe <79355446+Khadar3344@users.noreply.github.com>
2024-12-28 17:26:59 +0300

    AppNavigation update

--- app/src/main/java/com/khadarmustafe/agroconsult/navigation/AppNavigation.kt
+++ app/src/main/java/com/khadarmustafe/agroconsult/navigation/AppNavigation.kt
@@ -15,9 +15,17 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.khadarmustafe.agroconsult.ui.auth.screens.AlreadyFarmerScreen
+import com.khadarmustafe.agroconsult.ui.auth.screens.IntroScreen
+import com.khadarmustafe.agroconsult.ui.auth.screens.NewFarmerScreen
+import com.khadarmustafe.agroconsult.ui.auth.screens.RegionSelectionScreen
+import com.khadarmustafe.agroconsult.ui.auth.screens.SignInScreen
+import com.khadarmustafe.agroconsult.ui.auth.screens.SignUpScreen
+import com.khadarmustafe.agroconsult.ui.auth.screens.UserTypeScreen
 import com.khadarmustafe.agroconsult.ui.home.screens.CameraScreen
 import com.khadarmustafe.agroconsult.ui.home.screens.DashboardScreen
 import com.khadarmustafe.agroconsult.ui.home.screens.ImageScreen
+import com.khadarmustafe.agroconsult.ui.home.screens.SendSuccessfullyScreen
 import com.khadarmustafe.agroconsult.ui.home.screens.TextScreen
 import com.khadarmustafe.agroconsult.ui.home.screens.VideoRecorderScreen
 import com.khadarmustafe.agroconsult.ui.home.screens.VideoScreen
@@ -30,6 +38,13 @@ fun AppNavigation(
     controller: LifecycleCameraController,
     recordingVideo: (LifecycleCameraController) -> Unit,
     takePhoto: (LifecycleCameraController, (Bitmap) -> Unit) -> Unit,
+    onStartRecording: () -> Unit,
+    onStopRecording: () -> Unit,
+    onPlayRecording: () -> Unit,
+    onStopPlayback: () -> Unit,
+    onSendRecording: (String?) -> Unit,
+    recordingTime: Long,
+    recordingLength: Long
 ) {
     val navController = rememberNavController()
     val viewModel = viewModel<CameraViewModel>()
@@ -39,11 +54,31 @@ fun AppNavigation(
     val scope = rememberCoroutineScope()
     val scaffoldState = rememberBottomSheetScaffoldState()
 
-    NavHost(navController, startDestination = "dashboard") {
+    NavHost(navController, startDestination = "intro") {
+        composable("intro") { IntroScreen(navController) }
+        composable("sign_in") { SignInScreen(navController) }
+        composable("sign_up") { SignUpScreen(navController) }
+        composable("region_selection") { RegionSelectionScreen(navController) }
+        composable("user_type") { UserTypeScreen(navController) }
+        composable("new_farmer") { NewFarmerScreen(navController) }
+        composable("already_farmer") { AlreadyFarmerScreen(navController) }
         composable("dashboard") { DashboardScreen(navController) }
+        composable("send_successfully") { SendSuccessfullyScreen() }
         composable("image") { ImageScreen(title = "Image Screen", navController = navController, bitmap = currentBitmap) }
         composable("video") { VideoScreen(title = "Video Screen", navController = navController) }
-        composable("voice") { VoiceScreen(title = "Voice Screen", navController = navController) }
+        composable("voice") {
+            VoiceScreen(
+                title = "Voice Screen",
+                navController = navController,
+                onStartRecording = onStartRecording,
+                onStopRecording = onStopRecording,
+                onPlayRecording = onPlayRecording,
+                onStopPlayback = onStopPlayback,
+                onSendRecording = onSendRecording,
+                recordingTime = recordingTime,
+                recordingLength = recordingLength
+            )
+        }
         composable("text") { TextScreen(title = "Text Screen", navController = navController) }
 
         composable("camera") {
